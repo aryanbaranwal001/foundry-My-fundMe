cdde9221ea45b4f79ec4917826954697904bc5ff
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-16 00:39:02 +0530

    interactions error solved

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -24,7 +24,16 @@ contract fundFundMe is Script, StdCheats {
         // vm.startBroadcast();
         vm.deal(USER, 10 ether);
         console.log("user balance", USER.balance);
-        vm.broadcast(USER);
+
+        // I chatGPT my problem, he said to not use custom users they they don't exit in real anvil chain
+        // those addresses and vm.deal things happen in a simulated environment and not the actual anvil chain
+        // so to make a actual transaction call, I have to use the private key from anvil chain
+        // as it actually contains eth. Try running the following command in the terminal for both cases and see it for yourself.
+
+        // forge script script/Interactions.s.sol:fundFundMe --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast -vvvv 
+
+        vm.broadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+        // vm.broadcast(USER);
         fundMe.fund{value: 1 ether}();
         // vm.stopBroadcast();
         console.log("firstSender", fundMe.s_funders(0));
